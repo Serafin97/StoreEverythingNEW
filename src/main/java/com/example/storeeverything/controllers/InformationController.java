d8b3d46533908dc09607c91153dcb61b5380e5d1
Wojciech Bielawiec <wojciech.bielawiec@gmail.com>
2022-05-27 16:07:50 +0200

    sortowanie takie xDDD

--- src/main/java/com/example/storeeverything/controllers/InformationController.java
+++ src/main/java/com/example/storeeverything/controllers/InformationController.java
@@ -3,9 +3,13 @@ package com.example.storeeverything.controllers;
 import com.example.storeeverything.models.Information;
 import com.example.storeeverything.repositories.InformationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class InformationController {
@@ -17,12 +21,24 @@ public class InformationController {
     public String sharedInformations(Model model){
 
         model.addAttribute("informations", this.informationRepository.findAll());
+
+
         return "/informations/sharedinformations";
     }
 
-/*    @GetMapping({"/informations/myinformations"})
-    public String myInformations(Model model){
-        model.addAttribute("userList", this.informationRepository.findAll());
-        return "/informations/myinformations";
-    }*/
+    @GetMapping({"/informations/sharedinformations/{sort}"})
+    public String myInformations(@PathVariable int sort, Model model){
+        System.out.println(sort);
+        if(sort == 1) {
+            model.addAttribute("informations", informationRepository.findAll(PageRequest.of(0, 30, Sort.by(Sort.Direction.ASC, "addDate"))));
+        } else if (sort == 2) {
+            model.addAttribute("informations", informationRepository.findAll(PageRequest.of(0, 30, Sort.by(Sort.Direction.DESC, "addDate"))));
+        } else if (sort == 3) {
+            model.addAttribute("informations", informationRepository.findAll(PageRequest.of(0, 30, Sort.by(Sort.Direction.ASC, "category"))));
+        } else if (sort == 5) {
+            model.addAttribute("informations", informationRepository.findAll(PageRequest.of(0, 30, Sort.by(Sort.Direction.DESC, "addDate"))));
+        }
+
+        return "/informations/sharedinformations";
+    }
 }
