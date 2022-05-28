72cce90f9b2a78788710bb8ab6ffe9d5723b4e6e
Wojciech Bielawiec <wojciech.bielawiec@gmail.com>
2022-05-28 15:55:16 +0200

    start add informations

--- src/main/java/com/example/storeeverything/controllers/InformationController.java
+++ src/main/java/com/example/storeeverything/controllers/InformationController.java
@@ -1,27 +1,36 @@
 package com.example.storeeverything.controllers;
 
 import com.example.storeeverything.models.Information;
+import com.example.storeeverything.models.User;
 import com.example.storeeverything.repositories.InformationRepository;
-import com.example.storeeverything.security.IAuthenticationFacade;
-import com.example.storeeverything.security.MyUserDetails;
+import com.example.storeeverything.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-
+import org.springframework.web.bind.annotation.PostMapping;
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 @Controller
 public class InformationController {
 
     @Autowired
     private InformationRepository informationRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping({"/informations/sharedinformations"})
     public String sharedInformations(Model model){
@@ -83,4 +92,43 @@ public class InformationController {
 
         return "/informations/choseninformation";
     }
+
+    @GetMapping({"/informations/choseninformation/remove/{id}"})
+    public String chosenInformation(@PathVariable Long id){
+        informationRepository.deleteById(id);
+
+        return "redirect:/informations/myinformations";
+    }
+
+    @GetMapping({"/informations/addinformation"})
+    public String addInformationForm(){
+        return "/informations/addinformation";
+    }
+
+    @PostMapping({"/informations/addinformation"})
+    public String addInformation(@Valid @ModelAttribute("newinformation") Information information, BindingResult result, Model model, HttpServletRequest request){
+        if (result.hasErrors()){
+            return "/informations/addinformation";
+        }
+
+        LocalDate date = LocalDate.parse(LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+        information.setAddDate(date);
+
+        String username = request.getUserPrincipal().getName();
+        User user = userRepository.findByUsername(username);
+        information.setUser(user);
+
+        informationRepository.save(information);
+
+        return "redirect/informations/myinformations";
+    }
+
+
+
+    @GetMapping({"/informations/choseninformation/edit/{id}"})
+    public String editInformation(@PathVariable Long id){
+
+        return "/informations/editinformation";
+    }
+
 }
